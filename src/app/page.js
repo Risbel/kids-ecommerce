5abbbdb4a9e5cd38f5f131cb0f7b1acc00d642be
Risbel Suarez <risbel961019@gmail.com>
2023-05-26 15:38:09 -0400

    feat: Add basic project structure

--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen">
-      <div className="absolute bottom-0"></div>
-    </main>
-  );
-}
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="flex min-h-screen justify-center">
+      <h1>My e-commerce</h1>
+    </main>
+  );
+};
+
+export default Home;
