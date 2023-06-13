7792ab4adb2f491dc14010004c4647b42070c7ca
Risbel Suarez <risbel961019@gmail.com>
2023-06-13 17:20:27 -0400

    fix: Fix conditional on `ProductCard`

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,15 +11,15 @@ const ProductCard = ({ product }) => {
     <div className="text-center">
       <Link href={`/details/${product.slug}`}>
         <div className="relative">
-          {existItem?.quantity === product.countInStock ||
-            (product.countInStock === 0 && (
-              <div className="absolute z-20 right-2 top-2 bg-white rounded-3xl px-3 py-1">
-                <span className="truncate text-xs md:text-sm text-blue-800 font-semibold">out of stock</span>
-              </div>
-            ))}
+          {(product.countInStock === 0) && (
+            <div className="absolute z-20 right-2 top-2 bg-white rounded-3xl px-3 py-1">
+              <span className="truncate text-xs md:text-sm text-blue-800 font-semibold">out of stock</span>
+            </div>
+          )}
+
           <div className="relative overflow-hidden">
             <Image
-              className={`object-cover opacity-0 transition-opacity duration-700 ${
+              className={`object-cover opacity-0 transition-opacity duration-500 ${
                 product.morePictures && " hover:opacity-0 "
               }`}
               priority
@@ -33,7 +33,7 @@ const ProductCard = ({ product }) => {
             />
             {product.morePictures && (
               <Image
-                className="opacity-0 transition-opacity duration-500 absolute object-cover top-0 left-0 -z-10"
+                className="opacity-0 transition-opacity duration-1000 absolute object-cover top-0 left-0 -z-10"
                 width={580}
                 height={870}
                 alt={product.name}
@@ -49,7 +49,7 @@ const ProductCard = ({ product }) => {
 
       <h3 className="text-orange-600 font-semibold">{product.sexOrAge}</h3>
       <h2 className="text-gray-600 text-sm q  q">{product.description}</h2>
-      {existItem?.quantity !== product.countInStock && (
+      {product.countInStock > 0 && (
         <span className="font-bold text-gray-600">${product.price}.00</span>
       )}
     </div>
